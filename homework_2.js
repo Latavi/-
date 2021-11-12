f04ad6e8fc6614f838b9da06bf1aa3319f4966c1
Tatyana Laricheva <tatatv@yanex.tu>
2021-11-12 15:57:25 +0300

    Init commit

---
+++ homework_2.js
@@ -0,0 +1,20 @@
+    // "кот" > "код" => true
+	// "2" + 2 * "2" => 24
+	// undefined == null => false
+	// undefined === null => false
+	// null == 0 => false
+	// 2 > "3" => false
+	// null - false + true => 1
+	// 1 / "l" => NaN
+	// "2" * "3" => 6
+	// 4 + 5 + "O" => 9O
+	// "l" + 4 + 5 => l45
+	// "4" - 2 => 2
+	// "4" - "4x" => NaN
+	// null == false => true
+	// " -4 "/ 0 + 1 => -infiniti
+	// null + 1 => 1
+	// undefined + null => NaN
+	
+
+	
